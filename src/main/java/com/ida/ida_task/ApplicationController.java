5509ac70c9748f9c21d32b93ef062910bd8da9a8
Deepak <erdkgarg08@gmail.com>
2019-04-21 20:49:34 +0200

    Some improvement in code implementation

--- src/main/java/com/ida/ida_task/ApplicationController.java
+++ src/main/java/com/ida/ida_task/ApplicationController.java
@@ -16,7 +16,9 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.FileManager;
@@ -31,38 +33,42 @@ public class ApplicationController {
 	@RequestMapping("/create") //query is keyword.... CREATE  
 	public String getQuery3(@RequestBody recordclazz reco ) throws FileNotFoundException {
 		
-		System.out.println(reco.getQry());
-		System.out.println(reco.getEndpt());
+		System.out.println(reco.getFirst());
+		System.out.println(reco.getLast());
 		//System.out.println(reco.getQry());
 	    
         // some definitions
-        String personURI    = "http://somewhere/JohnSmith";
-        String givenName    = reco.getQry();
-        String familyName   = reco.getEndpt();
+        //String personURI    = "http://somewhere/JohnSmith";
+	
+        String givenName    = reco.getFirst();
+        String familyName   = reco.getLast();
         String fullName     = givenName + " " + familyName;
+        String personURI    = "http://userdata/"+fullName.replaceAll("\\s+","");
         // create an empty model
         Model model = ModelFactory.createDefaultModel();
 
         // create the resource 
         //   and add the properties cascading style
-        Resource johnSmith 
-          = model.createResource(personURI)
+        Resource rs 
+          = model.createResource(personURI)        		 
                  .addProperty(VCARD.FN, fullName)
                  .addProperty(VCARD.N, 
-                              model.createResource()
+                              model.createResource()                              	
                                    .addProperty(VCARD.Given, givenName)
                                    .addProperty(VCARD.Family, familyName));
         
-        
+        System.out.println("result set"+rs);
         // now write the model in XML form to a file
-        FileOutputStream fout=new FileOutputStream("data3.rdf");
-        		  model.write(fout,"RDF/XML");
-        model.write(System.out, "RDF/XML");
-		return fullName;    
-
-		
-
+        FileOutputStream fout=new FileOutputStream("data3.rdf",true);
+         Model model1 = ModelFactory.createDefaultModel();        
+        		  //model.write(fout,"RDF/XML");
+        		  
+        	  model1.add(model);
+        	  model1.write(fout,"RDF/XML");
+            //model.write(System.out, "RDF/XML");
+		return fullName;
 	}
+	
 	@RequestMapping("/query_2") //query is keyword 
 	public String getQuery(@RequestBody recordclazz rec ) {
 		
@@ -100,12 +106,38 @@ public class ApplicationController {
 	@RequestMapping("/fetch-all") //Fetch-all
 	public String getfetch() {
 		Model model = RDFDataMgr.loadModel("data3.rdf") ;
-		//RDFDataMgr.read(model, "http://host/some-published-data" ) ;
+		RDFDataMgr.read(model, "http://host/some-published-data" ) ;
 		return model.toString();
-	
+		
+//		// create an empty model
+//        Model model = ModelFactory.createDefaultModel();
+//       
+//        // use the FileManager to find the input file
+//        InputStream in = FileManager.get().open("data3.rdf");
+//        if (in == null) {
+//            throw new IllegalArgumentException( "File:  not found");
+//        }
+//        
+//        // read the RDF/XML file
+//        model.read( in, "");
+//        
+//        // select all the resources with a VCARD.FN property
+//        ResIterator iter = model.listResourcesWithProperty(VCARD.FN);
+//        if (iter.hasNext()) {
+//            System.out.println("The database contains vcards for:");
+//            while (iter.hasNext()) {
+//                System.out.println("  " + iter.nextResource()
+//                                              .getRequiredProperty(VCARD.FN)
+//                                              .getString() );
+//            }
+//        } else {
+//            System.out.println("No vcards were found in the database");
+//        }
+		//return null; 
 	}
+	
 	static final String inputFileName = "data3.rdf";
-   // static final String johnSmithURI = "http://somewhere/JohnSmith";
+    //static final String johnSmithURI = "http://somewhere/gargdeepak";
     
 	@RequestMapping("/fetch") //Fetch one by one
 	public String getfetchOneByOne(@RequestBody recordclazz records) {
@@ -121,7 +153,7 @@ public class ApplicationController {
         // read the RDF/XML file
         model.read(new InputStreamReader(in), "");
         
-        // retrieve the Adam Smith vcard resource from the model
+        // retrieve the vcard resource from the model
         Resource vcard = model.getResource(records.getQry());
         
         // retrieve the value of the N property
@@ -130,7 +162,7 @@ public class ApplicationController {
         // retrieve the given name property
         String fullName = vcard.getRequiredProperty(VCARD.FN)
                                .getString();
-        System.out.println("The nicknames of \"" + fullName + "\" are");
+        System.out.println("The fullname is \"" + fullName + "\" ");
 		return fullName;
 	}
 		
--- src/main/java/com/ida/ida_task/recordclazz.java
+++ src/main/java/com/ida/ida_task/recordclazz.java
@@ -2,6 +2,7 @@ package com.ida.ida_task;
 
 public class recordclazz {
 	private String qry,endpt;
+//	private int id;
 
 	public String getQry() {
 		return qry;
@@ -19,12 +20,33 @@ public class recordclazz {
 		this.endpt = endpt;
 	}
 
+	//user data 
 	 
-	 
-	 
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
 
-	 
+	private String first,last;
 
-	
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
 	
 }
