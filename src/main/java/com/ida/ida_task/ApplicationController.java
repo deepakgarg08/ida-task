a32f7c01b120c0183c516c3c8978d01a3ffa329e
Deepak <erdkgarg08@gmail.com>
2019-04-29 12:18:16 +0200

    checkpoint before adding sparql query

--- src/main/java/com/ida/ida_task/ApplicationController.java
+++ src/main/java/com/ida/ida_task/ApplicationController.java
@@ -16,10 +16,7 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.VCARD;
@@ -30,75 +27,73 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
-	@RequestMapping("/create") //query is keyword.... CREATE  
-	public String getQuery3(@RequestBody recordclazz reco ) throws FileNotFoundException {
-		
+	
+	@RequestMapping("/create")
+	public String getQuery3(@RequestBody recordclazz reco ) throws FileNotFoundException {		
 		System.out.println(reco.getUserName());
 		System.out.println(reco.getPassword());
 		//System.out.println(reco.getQry());
 	    
-        // some definitions
-		String userId =     reco.getId();
-        String userName    = reco.getUserName();
-        String password   = reco.getPassword();
-       // String fullName     = userName + " " + familyName;
-        String personURI    = "http://userdata/"+userName.replaceAll("\\s+","");
+		// some definitions
+	//	String userId = reco.getId();
+		String userName = reco.getUserName();
+		String password = reco.getPassword();
+		String personURI = "http://userdata/" + userName.replaceAll("\\s+", "");
+        
         // create an empty model
         Model model = ModelFactory.createDefaultModel();
-
         //   create the resource 
         //   and add the properties cascading style
         Resource rs 
           = model.createResource(personURI)
-        		 .addProperty(VCARD.UID, userId)
+        	//	 .addProperty(VCARD.UID, userId)
                  .addProperty(VCARD.NAME, userName)
                  .addProperty(VCARD.Pcode, password);
-                 //.addProperty(VCARD.N, 
-                           //   model.createResource()                              	
-                               //    .addProperty(VCARD.Given, userName)
-                                 //  .addProperty(VCARD.Family, familyName));
-        
+        //this is working fine
         System.out.println("result set"+rs);
         // now write the model in XML form to a file
-        FileOutputStream fout=new FileOutputStream("data3.rdf",true);
-         Model model1 = ModelFactory.createDefaultModel();        
+        FileOutputStream fout=new FileOutputStream("data3.ttl",true);
+      //  Model model1 = ModelFactory.createDefaultModel();        
         		  //model.write(fout,"RDF/XML");
         		  
-        	  model1.add(model);
-        	  model1.write(fout,"RDF/XML");
+        	  model.add(model);
+        	//  ((Object) model1).build()
+        	  model.write(fout,"Turtle");
+        	  
             //model.write(System.out, "RDF/XML");
-		return "The userId, userName and Password are:"+userId +userName + password;
+        	  
+		return "The userId, userName and Password are:" +userName + password;
 	}
-	
+	// get endpoint and query and return result
 	@RequestMapping("/query_2") //query is keyword 
 	public String getQuery(@RequestBody recordclazz rec ) {
 		
 		System.out.println(rec.getQry());
-		System.out.println(rec.getEndpt());
+	//	System.out.println(rec.getEndpt());
 
 		// Create Query
 		QueryFactory.create(rec.getQry());    
 
 		StringWriter modelAsString = new StringWriter();	
 
-		QueryExecution qexec = QueryExecutionFactory.sparqlService(rec.getEndpt(), rec.getQry());
+		QueryExecution qexec = QueryExecutionFactory.sparqlService("data3.ttl", rec.getQry());
 
 		Model resultss = qexec.execConstruct();
-		resultss.write(modelAsString, "RDF/XML");
+		resultss.write(modelAsString, "turtle");
 		String sparql_result = modelAsString.toString();
 
 		System.out.println("-"+sparql_result);
 
-		File file = new File("data3.rdf");
-		BufferedWriter writer;
-		try {
-			writer = new BufferedWriter(new FileWriter(file));
-			writer.write(sparql_result);
-			writer.close();
-
-		} catch (IOException e) {		
-			e.printStackTrace();
-		}
+//		File file = new File("data3.ttl");
+//		BufferedWriter writer;
+//		try {
+//			writer = new BufferedWriter(new FileWriter(file));
+//			writer.write(sparql_result);
+//			writer.close();
+//
+//		} catch (IOException e) {		
+//			e.printStackTrace();
+//		}
 
 		return sparql_result;		 
 
@@ -106,7 +101,7 @@ public class ApplicationController {
 	
 	@RequestMapping("/fetch-all") //Fetch-all
 	public String getfetch() {
-		Model model = RDFDataMgr.loadModel("data3.rdf") ;
+		Model model = RDFDataMgr.loadModel("data9.rdf") ;
 		RDFDataMgr.read(model, "http://host/some-published-data" ) ;
 		return model.toString();
 		
@@ -137,7 +132,7 @@ public class ApplicationController {
 //return null; 
 	}
 	
-	static final String inputFileName = "data3.rdf";
+	static final String inputFileName = "data3.ttl";
     //static final String johnSmithURI = "http://somewhere/gargdeepak";
     
 	@RequestMapping("/fetch") //Fetch one by one
